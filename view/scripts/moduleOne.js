022627bc997050f8173940811fb543b8a99674c3
SMC LPZ <smclpz@smcbolivia.local>
2025-01-30 15:43:10 -0400

    commit4

--- view/scripts/moduleOne.js
+++ view/scripts/moduleOne.js
@@ -1,20 +1,20 @@
-const value_edf1_0 = document.getElementById('value-edf1-0');
-const value_edf1_1 = document.getElementById('value-edf1-1');
-const value_edf1_2 = document.getElementById('value-edf1-2');
-const value_edf1_3 = document.getElementById('value-edf1-3');
-const value_edf1_4 = document.getElementById('value-edf1-4');
-const value_edf1_5 = document.getElementById('value-edf1-5');
+const values = [];
+for (let i = 0; i <= 46; i++) {
+  values.push(document.getElementById(`value_edf1_${i}`));
+}
+
 setInterval(() => {
     fetch('../ttr_edf1', {
         method: "POST"
     }).then(response => response.json()).then(data => {
-        data[0].VR.forEach(element => {
-            value_edf1_0.textContent = `${values[0].toFixed(2)} V`;
-            value_edf1_1.textContent = `${values[1].toFixed(2)} V`;
-            value_edf1_2.textContent = `${values[2].toFixed(2)} V`;
-            value_edf1_3.textContent = `${values[3].toFixed(2)} V`;
-            value_edf1_4.textContent = `${values[4].toFixed(2)} V`;
-            value_edf1_5.textContent = `${values[5].toFixed(2)} V`;
-        });
+        const pac3200 = data[0].VR.slice(0, 21).concat(
+            data[0].VR.slice(27, 28),
+            data[0].VR.slice(31, 35),
+            data[0].VR.slice(37,58)
+        );
+        pac3200 = array1.concat(array2).concat(array3).concat(array4);
+        for (let i = 0; i < values.length; i++) {
+            values[i].innerHTML = pac3200[i].toFixed(2);
+        }
     }).catch(err => console.log(err));
 }, 1000);
